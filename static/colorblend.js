1e4cc080123e1a1a2454c5744064c14b53f7fff2
jskelcy <jskelcy@gmail.com>
2014-04-24 14:43:49 -0400

    common factors

--- static/colorblend.js
+++ static/colorblend.js
@@ -32,12 +32,15 @@ $(document).ready(function(){
 });
 
 
-
+//this function  will take in specified grid infomation form user
+//It will then draw the grid onto the canvas  and return the color values 
+//the next thing that needs to happen is to take the numRows and numColumns and make that come from user input
 function getGrid(squareWidth,squareHeight, ctx){
     var gridValues =[];
     var yPosition = 0;
-    var numRows = 10;
-    var numColumns = 10; 
+    //these lines need to be arugents
+    var numRows = 38;
+    var numColumns = 21; 
     //vertical
     for(var row = 0; row < numRows; ++row){
         xPosition =0;
@@ -54,16 +57,32 @@ function getGrid(squareWidth,squareHeight, ctx){
     return gridValues;
 }
 
-
+//This function creates a new canvas after all the data has been collected from the 'getGrid' function 
+//It well then draw a rectangles with the color in the colorData array.
+//right now we are multiplying things by 50. I dont know why. But at some point we will need to take in the 
+//numRows and numColumns data and use that tell the ctx where to print
 function render(colorData,canvasWidth,canvasHeight){
+    var squareSize = 50;
     var newCanvas = $('<canvas/>',{'id': 'printCanvas', 'wiDth': canvasWidth, 'hEight': canvasHeight});
     $('#canvases').append(newCanvas)
     var ctx = newCanvas[0].getContext('2d');
-     
+    //DONT FORGET: the yPos and xPos are relative to the array, not to the position where the rectangle is being printed on the canvas
    for(var yPos = 0, yLen = colorData.length; yPos < yLen; yPos++){
         for(var xPos = 0, xLen = colorData[yPos].length; xPos < xLen; xPos++){
+            console.log(xPos, yPos)
             ctx.fillStyle ='rgb(' + colorData[xPos][yPos].R +',' + colorData[xPos][yPos].G +',' + colorData[xPos][yPos].B+')';
-            ctx.fillRect(yPos*50,xPos*50,50,50);
+            ctx.fillRect(yPos*squareSize,xPos*squareSize,squareSize,squareSize);
        }
     }
+}
+
+
+function commonFactors(value1, value2){
+    var min = Math.min(value1, value2);
+    var commonArray = _.filter(_.range(1, min+1),function(x){
+        if(value1%x === 0 && value2%x === 0){
+            return true
+        }
+    })
+    return commonArray
 }
